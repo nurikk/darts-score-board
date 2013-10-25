4907766ff8b193ee851fb461569cde7c87c53340
Ainur <nur.php@gmail.com>
2013-10-25 18:14:02 +0700

    more usability

--- static/app.js
+++ static/app.js
@@ -10,14 +10,23 @@ angular.module('score').controller('scoreController', function ($scope){
   $scope.add_user = function (){
     $scope.users.push({
       name: 'user_' + ($scope.users.length + 1),
-      score: $scope.default_score
+      score: $scope.default_score,
+      scores: []
     });
   };
 });
 
 angular.module('score').controller('addScoreController', function ($scope){
   $scope.add_score = function (user){
-    var new_score = user.score - $scope.score;
+    var score = ~~$scope.score;
+    if ($scope.score.indexOf(' ') != -1) {
+      score = 0;
+      angular.forEach($scope.score.split(' '), function (val){
+        score += ~~val;
+      });
+    }
+
+    var new_score = user.score - score;
     if (new_score == 1 || new_score < 0) {
       alert('error');
     } else {
